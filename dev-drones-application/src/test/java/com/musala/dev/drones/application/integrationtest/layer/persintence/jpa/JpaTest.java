46c0058dda10a02174ef1aa66f26971eb86884b8
ktikhonov <akirilltikhonov@gmail.com>
2023-09-17 14:24:21 +0300

    don't use import

--- dev-drones-application/src/test/java/com/musala/dev/drones/application/integrationtest/layer/persintence/jpa/JpaTest.java
+++ dev-drones-application/src/test/java/com/musala/dev/drones/application/integrationtest/layer/persintence/jpa/JpaTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Propagation;
@@ -15,8 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
-@Import(TestJpaConfiguration.class)
-@ContextConfiguration(initializers = PostgresTestcontainerInitializer.class)
+@ContextConfiguration(initializers = PostgresTestcontainerInitializer.class, classes = TestJpaConfiguration.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Rollback(value = false)
 @Transactional(propagation = Propagation.NEVER)
